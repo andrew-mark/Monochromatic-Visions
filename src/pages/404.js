b59c73b0f734472348e6ec2d557515878ea4d407
Andrew Butler <a.butler@sp.design>
2020-05-10 00:13:09 +0200

    remove splash page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Components from './components.js';
+
+const PageNotFound = (props) => {
+  return (
+    <React.Fragment>
+      hello
+    </React.Fragment>
+  )
+}
+
+export default PageNotFound
