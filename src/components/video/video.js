aaa68b5639a5f4afe476da4f55fb64195aed3978
Andrew Butler <a.butler@sp.design>
2020-06-03 12:10:50 +0200

    change video to gif

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -1,13 +1,12 @@
 import React, {useState, useContext} from 'react'
 import './video.scss'
 import Waves from '../../static/waves-split.mp4'
+import Water from '../../static/water.gif'
 
 const Video = (props) => {
   return (
     <div className={`video`}>
-      <video muted defaultMuted autoPlay loop playsInline className="video-video">
-        <source src={Waves} type="video/mp4"></source>
-      </video>
+      <img src={Water}  className="video-video"/>
     </div>
   )
 }
