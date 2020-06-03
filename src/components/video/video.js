ecef5c2c7dffd7cd82fb7253152f1be1593e728d
Andrew Butler <a.butler@sp.design>
2020-06-03 12:32:16 +0200

    change gif to image

--- src/components/video/video.js
+++ src/components/video/video.js
@@ -1,12 +1,12 @@
 import React, {useState, useContext} from 'react'
 import './video.scss'
 import Waves from '../../static/waves-split.mp4'
-import Water from '../../static/water.gif'
+import Water from '../../static/water.jpg'
 
 const Video = (props) => {
   return (
     <div className={`video`}>
-      <img src={Water}  className="video-video"/>
+      <img src={Water} alt="water-flowing" className="video-video"/>
     </div>
   )
 }
